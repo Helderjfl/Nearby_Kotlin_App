b70b405e696482d172984eabac15933d36b75479
Helder Jefferson <Helderjfl@users.noreply.github.com>
2025-02-14 19:21:53 -0300

    Feat: Maps and working with the API

--- app/src/main/java/com/example/nearby/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/nearby/ui/screen/home/HomeScreen.kt
@@ -1,7 +1,6 @@
-package com.example.nearby.ui.screen
+package com.example.nearby.ui.screen.home
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,75 +9,82 @@ import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.nearby.data.model.Market
-import com.example.nearby.data.model.mock.mockCategories
-import com.example.nearby.data.model.mock.mockMarkets
 import com.example.nearby.ui.components.category.NearbyCategoryFilterChipList
+import com.example.nearby.ui.components.home.NearbyGoogleMap
 import com.example.nearby.ui.components.market.NearbyMarketCardList
 import com.example.nearby.ui.theme.Gray100
-import com.google.maps.android.compose.GoogleMap
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier, onNavigateToMarketDetails: (Market) -> Unit) {
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    uiState: HomeUiState,
+    onEvent: (HomeUiEvent) -> Unit,
+    onNavigateToMarketDetails: (Market) -> Unit
+) {
     val bottomSheetState = rememberBottomSheetScaffoldState()
-    var isBottomSheetOpened by remember { mutableStateOf(true) }
 
     val configuration = LocalConfiguration.current
 
-    if (isBottomSheetOpened) {
-        BottomSheetScaffold(
-            modifier = modifier,
-            scaffoldState = bottomSheetState,
-            sheetContainerColor = Gray100,
-            sheetPeekHeight = configuration.screenHeightDp.dp / 3,
-            sheetContent = {
+    LaunchedEffect(true) {
+        onEvent(HomeUiEvent.OnFetchCategories)
+    }
+
+    BottomSheetScaffold(
+        modifier = modifier,
+        scaffoldState = bottomSheetState,
+        sheetContainerColor = Gray100,
+        sheetPeekHeight = configuration.screenHeightDp.dp / 3,
+        sheetContent = {
+            if (!uiState.markets.isNullOrEmpty())
                 NearbyMarketCardList(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(16.dp),
-                    markets = mockMarkets,
+                    markets = uiState.markets,
                     onMarketClick = { selectedMarket ->
                         onNavigateToMarketDetails(selectedMarket)
                     }
                 )
-            },
-            sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
-            content = {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(it)
-                ) {
-                    GoogleMap(
-                        modifier = Modifier.fillMaxSize(),
-                    )
+        },
+        sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+        content = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = it.calculateBottomPadding().minus(16.dp))
+            ) {
+                NearbyGoogleMap(
+                    modifier = Modifier.fillMaxSize(),
+                    uiState = uiState
+                )
+
+                if (!uiState.categories.isNullOrEmpty())
                     NearbyCategoryFilterChipList(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(top = 24.dp)
+                            .padding(top = 32.dp)
                             .align(Alignment.TopStart),
-                        categories = mockCategories,
-                        onSelectedCategoryChanged = {}
+                        categories = uiState.categories,
+                        onSelectedCategoryChanged = { selectedCategory ->
+                            onEvent(HomeUiEvent.OnFetchMarkets(categoryId = selectedCategory.id))
+                        }
                     )
-                }
-
             }
-        )
-    }
+
+        }
+    )
 }
 
 @Preview
 @Composable
 private fun HomeScreenPreview() {
-    HomeScreen(onNavigateToMarketDetails = {})
+    HomeScreen(onNavigateToMarketDetails = {}, uiState = HomeUiState(), onEvent = {})
 }
--- app/src/main/java/com/example/nearby/ui/screen/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/example/nearby/ui/screen/welcome/WelcomeScreen.kt
@@ -1,34 +1,21 @@
-package com.example.nearby.ui.screen
+package com.example.nearby.ui.screen.welcome
 
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.nearby.R
 import com.example.nearby.ui.components.button.NearbyButton
 import com.example.nearby.ui.components.welcome.WelcomeContent
 import com.example.nearby.ui.components.welcome.WelcomeHeader
-import com.example.nearby.ui.theme.Gray500
-import com.example.nearby.ui.theme.RedBase
-import com.example.nearby.ui.theme.Typography
 
 @Composable
 fun WelcomeScreen(modifier: Modifier = Modifier, onNavigateToHome: () -> Unit) {
